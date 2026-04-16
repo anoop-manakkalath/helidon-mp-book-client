6bae256d029093fdcf7e3eccfb5c26317c9e1b55
Anoop Manakkalath <anoop.manakkalath@gmail.com>
2026-04-16 22:34:24 +0530

    Complete decoipling of entity and dto. Other fixes.

--- src/main/java/com/app/book/mapper/BookMapper.java
+++ src/main/java/com/app/book/mapper/BookMapper.java
@@ -1,15 +1,11 @@
 package com.app.book.mapper;
 
-import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 
 import com.app.book.dto.Book;
 import com.app.book.model.BookEntity;
 
-@Mapper(
-	componentModel = "cdi",
-	builder = @Builder(disableBuilder = false)
-)
+@Mapper(componentModel = "cdi")
 public interface BookMapper {
 
     Book toDto(BookEntity entity);
